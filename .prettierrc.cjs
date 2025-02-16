ff2bcfeb9e94d9687d0b1dd3ff03f3e8b496502b
sohail-fabtech <sohail.khan@fabtechsol.com>
2025-02-16 21:56:29 +0500

    add

---
+++ .prettierrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  jsxSingleQuote: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "avoid",
+  htmlWhitespaceSensitivity: "css",
+  vueIndentScriptAndStyle: false,
+  endOfLine: "auto",
+};
+
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div className="w-full h-screen bg-zinc-900">starter app</div>;
+}
+
+export default App;
