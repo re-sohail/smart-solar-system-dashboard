3ac8d5723a166f0391be029269d3bf40c4a9627c
sohail-fabtech <sohail.khan@fabtechsol.com>
2025-02-21 22:22:16 +0500

    Fix vercel 404 issue

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button';
 import React from 'react';
 
 function NotFoundPage() {
   return (
-    <div className='w-full h-screen bg-zinc-900 text-white flex items-center justify-center text-xl font-semibold'>
-      NotFoundPage
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+      <h1 className='text-4xl font-bold'>404 - Page Not Found</h1>
+      <p className='mt-4 text-lg'>The page you're looking for doesn't exist.</p>
     </div>
   );
 }
